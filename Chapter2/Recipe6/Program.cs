cfb93edb05f62e99066fe5a0017dafa05a3367ee
PanFei <pfei1214@163.com>
2023-05-29 16:55:36 +0800

    使用CountdownEvent类

---
+++ Chapter2/Recipe6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace Recipe6
+{
+    /**
+     * 使用CountDownEvent类
+     * 
+     * 使用CountDownEvent信号类来等待直到一定数量的操作完成
+     */
+    internal class Program
+    {
+        /**
+         * 当主程序启动时，创建了一个CountdownEvent实例，在其构造函数中指定了当两个操作完成时会发出信号。
+         * 然后我们启动了两个线程，当它们执行完成后会发出信号。
+         * 一旦第二个线程完成，主线程会从等待CountdownEvent的状态中返回并继续执行。
+         * 针对需要等待多个异步操作完成的情形，使用该方式是非常便利的。
+         * 
+         * 然而这有一个重大的缺点。
+         * 如果调用_countdown。Signal()没达到指定的次数，那么_countdown.Wait()将一直等待。
+         * 请确保使用CountdownEvent时，所有线程完成后都要调用Signal方法。
+         * 
+         */
+        static void Main(string[] args)
+        {
+            WriteLine(DateTime.Now.ToString("ss") + ":  Starting two operations");
+            Thread t1 = new Thread(() => PerformOperation("Operation 1 is completed", 4));
+            Thread t2 = new Thread(() => PerformOperation("Operation 2 is completed", 8));
+            t1.Start();
+            t2.Start();
+            _countdown.Wait();
+            WriteLine(DateTime.Now.ToString("ss") + ":  Both operations have been completed.");
+            _countdown.Dispose();
+
+            ReadKey();
+        }
+
+        /// <summary>
+        /// 表示在计数变为零时处于有信号状态的同步基元。
+        /// 默认：最初所需设置信号的数量为2
+        /// </summary>
+        static CountdownEvent _countdown = new CountdownEvent(2);
+        static void PerformOperation(string message,int seconds)
+        {
+            Sleep(TimeSpan.FromSeconds(seconds));
+            WriteLine(DateTime.Now.ToString("ss") + ":  " + message);
+            _countdown.Signal();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -19,6 +19,7 @@
     - [Recipe3 使用semaphoreSlim类](#recipe3-semaphoreslim)
     - [Recipe4 使用AutoResetEvent类](#recipe4-autoresetevent)
     - [Recipe5 使用ManualResetEventSlim类](#recipe5-manualreseteventslim)
+    - [Recipe6 使用CountdownEvent类](#recipe6-countdownevent)
   - [<span id="Tips">知识点</span>](#span-idtipsspan)
 <!--/TOC-->
 
@@ -94,6 +95,17 @@
 > 然而线程3还处于睡眠状态，没有赶上时间。<br/>
 > 当调用_mainEvent.Reset相当于关闭了大门。<br/>
 > 最后一个线程已经准备好执行，但是不得不等待下一个信号，即要等待好几秒种。
+
+### Recipe6 使用CountdownEvent类
+
+>当主程序启动时，创建了一个CountdownEvent实例，在其构造函数中指定了当两个操作完成时会发出信号。<br/>
+>然后我们启动了两个线程，当它们执行完成后会发出信号。<br/>
+>一旦第二个线程完成，主线程会从等待CountdownEvent的状态中返回并继续执行。<br/>
+>针对需要等待多个异步操作完成的情形，使用该方式是非常便利的。
+
+>然而这有一个重大的缺点。<br/>
+>如果调用_countdown。Signal()没达到指定的次数，那么_countdown.Wait()将一直等待。<br/>
+>请确保使用CountdownEvent时，所有线程完成后都要调用Signal方法。
 
 <br/>
 
